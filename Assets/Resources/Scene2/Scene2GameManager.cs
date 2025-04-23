8cdd6b4817939f6c9affe7ebf88cef2b193bf5f9
huijung <huijung2097@naver.com>
2025-04-24 00:32:00 +0900

    1차 커밋

--- Assets/Resources/Scene2/Scene2GameManager.cs
+++ Assets/Resources/Scene2/Scene2GameManager.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class Scene2GameManager : MonoBehaviour
 {
-    
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    public static void CustomAfterSceneLoad()
+    {
+        
+    }
 }
