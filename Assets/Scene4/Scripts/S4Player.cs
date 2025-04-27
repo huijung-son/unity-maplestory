e5c788e167a69199320e15327d69ed631b8ed754
hyeonbao <halee2468@naver.com>
2025-04-28 08:27:48 +0900

    add char moving

--- Assets/Scene4/Scripts/S4Player.cs
+++ Assets/Scene4/Scripts/S4Player.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class S4Player : MonoBehaviour
 {
-    public float moveSpeed = 3f;
+    private float moveSpeed = 3f;
+    private float jumpPower = 5f;
     private Rigidbody2D rb;
+    private bool isGrounded = false;
 
     private void Awake()
     {
@@ -13,30 +15,53 @@ public class S4Player : MonoBehaviour
     private void Update()
     {
         Moving();
+        Jump();
+        RopeCatch();
     }
 
     private void Moving()
     {
-        Vector2 movement = Vector2.zero;
+        Vector2 moveVelocity = Vector2.zero;
+        float inputX = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKey(KeyCode.UpArrow))
-            {
-            movement.y = 1;
-            }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        if (inputX < 0)
         {
-            movement.y = -1;
+            moveVelocity = Vector2.left;
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (inputX > 0)
+        {
+            moveVelocity = Vector2.right;
+            transform.localScale = new Vector2(-1, 1);
+        }
+        rb.linearVelocity = new Vector2(moveVelocity.x * moveSpeed, rb.linearVelocity.y);
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (collision.gameObject.CompareTag("Rope"))
         {
-            movement.x = -1;
-        }
-        else if ( Input.GetKey(KeyCode.RightArrow))
-        {
-            movement.x = 1;
-        }
 
-        rb.linearVelocity = movement.normalized * moveSpeed;
+        }
+    }
+
+    private void RopeCatch()
+    {
+        
     }
 }
