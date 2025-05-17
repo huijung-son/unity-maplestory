2f71d33f3525ab9eefa47ebb63485bd8903d0fab
coderHyeon <boksil211@kakao.com>
2025-05-17 20:19:29 +0900

    Revert "[Feat] 시작화면 추가"
    This reverts commit c55fa6a70f86e10c0ad9336113aac75a85321758.

--- Assets/Fallguys/SHJ/Scripts/NetLocalPlayerMoving.cs
+++ Assets/Fallguys/SHJ/Scripts/NetLocalPlayerMoving.cs
@@ -1,9 +1,106 @@
-public class FG_GameStateOnlyPSH
-{ 
+using Unity.Netcode;
+using UnityEngine;
 
-    public static void Play() { curState = EGameState.Play; }
-    public static void OpenUI() { curState = EGameState.OpenUI; }
+public class NetLocalPlayerMoving : NetworkBehaviour
+{
+    [Header("camera offset")]
+    public Vector3 cameraOffset = new Vector3(0f, 2f, -4f);
 
-    public static bool IsPlay() { return curState == EGameState.Play; }
-    public static bool IsOpenUI() { return curState == EGameState.OpenUI; }
+    private Camera mainCam;
+    private Rigidbody rb = null;
+    
+    private float rotX = 0f;
+    private float rotY = 0f;
+    private float mouseSensitivity = 130f;
+
+    private float moveSpeed = 5f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        if (!IsOwner) return;
+
+        mainCam = Camera.main;
+        if (mainCam == null)
+        {
+            Debug.LogError("MainCamera를 찾을 수 없습니다!");
+            return;
+        }
+
+        Transform camT = mainCam.transform;
+        camT.SetParent(transform);
+        camT.localPosition = cameraOffset;
+        camT.localRotation = Quaternion.identity;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        rotX = transform.rotation.eulerAngles.x;
+        rotY = camT.rotation.eulerAngles.y;
+    }
+    
+    private void Update()
+    {
+        float axisH = Input.GetAxis("Horizontal");
+        float axisV = Input.GetAxis("Vertical");
+        PlayerMoving(axisH, axisV);
+        
+        if (rb != null && Input.GetKeyDown(KeyCode.Space)) {
+            Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(10f * -Physics.gravity.y);
+            rb.AddForce(jumpVelocity, ForceMode.Impulse);
+        }
+    }
+
+    private void PlayerYaw(float _axisX)
+    {
+        float newRotX = rotX + (_axisX * mouseSensitivity);
+        rotX = Mathf.Lerp(rotX, newRotX, Time.deltaTime);
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot.y = rotX;
+        transform.rotation = Quaternion.Euler(rot);
+    }
+
+    private void CameraPitch(float _axisY)
+    {
+        float newRotY = rotY - (_axisY * mouseSensitivity);
+        rotY = Mathf.Lerp(rotY, newRotY, Time.deltaTime);
+        Vector3 rot = mainCam.transform.rotation.eulerAngles;
+        rot.x = rotY;
+        mainCam.transform.rotation = Quaternion.Euler(rot);
+    }
+
+    private void PlayerMoving(float _axisH, float _axisV)
+    {
+        if (!IsOwner || mainCam == null) return;
+        
+        Vector3 moveH = mainCam.transform.right * _axisH;
+        moveH.y = 0f;
+        moveH.Normalize();
+        Vector3 moveV = mainCam.transform.forward * _axisV;
+        moveV.y = 0f;
+        moveV.Normalize();
+
+        Vector3 moveDir = (moveH + moveV).normalized;
+
+        transform.position += (moveSpeed * Time.deltaTime * moveDir);
+    }
+
+    void LateUpdate()
+    {
+        if (!IsOwner || mainCam == null) return;
+        
+        float axisX = Input.GetAxis("Mouse X");
+        float axisY = Input.GetAxis("Mouse Y");
+        PlayerYaw(axisX);
+        CameraPitch(axisY);
+        // // 부드러운 따라가기 로직 원하면 Lerp 등 추가
+        // Transform camT = mainCam.transform;
+        // Vector3 desiredPos = transform.TransformPoint(cameraOffset);
+        // camT.position = Vector3.Lerp(camT.position, desiredPos, Time.deltaTime * 10f);
+        // camT.LookAt(transform.position + Vector3.up * 1.5f);  // 시선 높이 조정
+    }
 }
